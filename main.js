becddf970a2bba361e3506a0c17733cae2778d0a
Arthur <avprocopio@gmail.com>
2023-10-11 18:17:51 -0300

    after end of first stream

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const getBannerData = async () => {
+  const response = await fetch('https://cdn.jsdelivr.net/gh/arthurproc/styled-banners-twitch-extension/banner-info-example.json');
+  const data = await response.json();
+  return data;
+};
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+const loadBanner = async () => {
+  const data = await getBannerData();
+  const wrapper = document.querySelector('.banner-wrapper');
+  const title = document.querySelector('.banner-title');
+  const description = document.querySelector('.banner-description');
+  const imageWrapper = document.querySelector('.banner-image-wrapper');
+
+
+  title.innerText = data.title;
+  description.innerText = data.description;
+  const img = document.createElement('img');
+  img.src = data.image;
+  imageWrapper.appendChild(img);
+
+  if (data.cta) {
+    const cta = document.createElement('a');
+    cta.href = data.cta.link;
+    cta.innerText = data.cta.text;
+    wrapper.appendChild(cta);
+  }
+
+  if (data.cssFile) {
+    console.log('css file');
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = 'banner-custom-stylesheet';
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = data.cssFile;
+    link.media = 'all';
+    head.appendChild(link);
+  }
+}
+
+loadBanner();
